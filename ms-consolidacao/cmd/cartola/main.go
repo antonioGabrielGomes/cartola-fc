bcbdfdb2a6ebc4549a738c90c4685875bd62d7eb
antonioGabrielGomes <antonioggca000@gmail.com>
2022-12-07 00:31:19 -0300

    correção

--- ms-consolidacao/cmd/cartola/main.go
+++ ms-consolidacao/cmd/cartola/main.go
@@ -7,8 +7,10 @@ import (
 
 	"github.com/antonioGabrielGomes/cartola-fc/ms-consolidacao/internal/infra/db"
 	httphandler "github.com/antonioGabrielGomes/cartola-fc/ms-consolidacao/internal/infra/http"
+	"github.com/antonioGabrielGomes/cartola-fc/ms-consolidacao/internal/infra/kafka/consumer"
 	"github.com/antonioGabrielGomes/cartola-fc/ms-consolidacao/internal/infra/repository"
 	"github.com/antonioGabrielGomes/cartola-fc/ms-consolidacao/pkg/uow"
+	"github.com/confluentinc/confluent-kafka-go/kafka"
 	"github.com/go-chi/chi"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -16,7 +18,7 @@ import (
 
 func main() {
 	ctx := context.Background()
-	dtb, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/cartola?parseTime=true")
+	dtb, err := sql.Open("mysql", "root:root@tcp(mysql:3306)/cartola?parseTime=true")
 	if err != nil {
 		panic(err)
 	}
@@ -34,9 +36,12 @@ func main() {
 	r.Get("/matches", httphandler.ListMatchesHandler(ctx, repository.NewMatchRepository(dtb)))
 	r.Get("/matches/{matchID}", httphandler.ListMatchByIDHandler(ctx, repository.NewMatchRepository(dtb)))
 
-	if err = http.ListenAndServe(":8080", r); err != nil {
-		panic(err)
-	}
+	go http.ListenAndServe(":8081", r)
+
+	var topics = []string{"newMatch", "chooseTeam", "newPlayer", "matchUpdateResult", "newAction"}
+	msgChan := make(chan *kafka.Message)
+	go consumer.Consume(topics, "broker:9094", msgChan)
+	consumer.ProcessEvents(ctx, msgChan, uow)
 }
 
 func registerRepositories(uow *uow.Uow) {
